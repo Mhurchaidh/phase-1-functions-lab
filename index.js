351fa179908df4c8d4d7b85a84360494ef602d0e
mhurchaidh <bmurchie1997@gmail.com>
2022-12-19 10:23:43 -0800

    commit tree 50ff150b3541a9bc9e67e99e2fdb7576de4d2692

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+    const hq = 42;
+    if(pickUpLocation >= hq){
+        return pickUpLocation - hq;
+    }
+    return hq - pickUpLocation;
+    
+}
+
+function distanceFromHqInFeet(blockDistance){
+    return distanceFromHqInBlocks(blockDistance) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(start >= destination){
+        return (start - destination) * 264;
+    }
+    return (destination - start) * 264;
+}
+
+function calculatesFarePrice(startLocation, endLocation){
+    const dist = distanceTravelledInFeet(startLocation, endLocation);
+    switch(true){
+        case dist <= 400:
+            return 0;
+        case dist > 400 && dist <= 2000:
+            return (dist - 400) * 0.02;
+        case dist > 2000 && dist <= 2500:
+            return 25;
+        case dist > 2500:
+            return 'cannot travel that far';
+    }
+}
+
+
